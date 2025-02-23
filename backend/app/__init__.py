db20325ad0a87f2556e22f0cb60730e10c58f364
IndifferentYk <1154299769@qq.com>
2025-02-23 16:24:14 +0800

    backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from openai import OpenAI
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
+    client = OpenAI(
+        base_url = app.config['OLLAMA_URL'],
+        api_key='ollama',
+    )
+    from .models import set_client
+    set_client(client)
 
     from . import routes
     app.register_blueprint(routes.bp)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from .functions import get_air_quality
+from .functions import get_air_quality, get_navigation, get_nearby
+from .models import adjust_aircon
 
 bp = Blueprint('main', __name__)
 
@@ -12,14 +13,18 @@ def airquality(address):
     response = get_air_quality(address)
     return response
 
-@bp.route('/navigation')
-def navigation():
-    return 'Navigation'
+@bp.route('/navigation/<origin>/<destination>')
+def navigation(origin, destination):
+    response = get_navigation(origin, destination)
+    return response
 
-@bp.route('/nearby')
-def nearby():
-    return 'Nearby'
+@bp.route('/nearby/<address>/<keyword>')
+def nearby(address, keyword):
+    response = get_nearby(address, keyword)
+    return response
 
-@bp.route('/facilities')
-def facilities():
-    return 'Facilities'
+
+@bp.route('/facilities/<user_prompt>')
+def facilities(user_prompt):
+    response = adjust_aircon(user_prompt)
+    return response
