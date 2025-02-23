0792a2d8a7c56eb2f217f0fd17aea79f6bac219a
IndifferentYk <1154299769@qq.com>
2025-02-23 14:50:31 +0800

    Update .gitignore

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    
-
-    GOOGLE_GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
-    GOOGLE_AIR_QUALITY_URL = "https://airquality.googleapis.com/v1/currentConditions:lookup?key={API_KEY}"
-    GOOGLE_SEARCH_NEARBY_URL = "https://places.googleapis.com/v1/places:searchNearby"
-
-    OLLAMA_URL = "http://localhost:11434/v1/"
