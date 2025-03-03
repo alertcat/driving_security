299a79d334c7cdfd1f0792f38421afb7ca8d2a2d
IndifferentYk <1154299769@qq.com>
2025-03-03 14:48:20 +0800

    Add RAGFlow

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from openai import OpenAI
 from langchain_ollama import OllamaLLM
+from ragflow_sdk import RAGFlow
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +16,10 @@ def create_app():
     # )
     from .models import set_client
     set_client(client)
+
+    from .models import set_session
+    rag_object = RAGFlow(api_key=app.config['RAGFLOW_API_KEY'], base_url=app.config["RAGFLOW_URL"]+":9380")
+    set_session(rag_object)
 
     from . import routes
     app.register_blueprint(routes.bp)
